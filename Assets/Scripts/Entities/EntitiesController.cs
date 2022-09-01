6dc80a9f37eb3c34ce59d7fd43a5a3e1b0d1837d
CamiloAmihan <camilo@amihan.gg>
2022-08-31 21:21:17 -0500

    Added a list to entitiescontroller

--- Assets/Scripts/Entities/EntitiesController.cs
+++ Assets/Scripts/Entities/EntitiesController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EntitiesController : MonoBehaviour
 {
-
+    private List<IEntity> m_entities;
 
     // Start is called before the first frame update
     void Start()
