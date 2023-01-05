a25ff888f2a42a59bcb21d4f417926303d3cb40b
CamiloHoyos16 <acamilohoyos@hotmail.com>
2023-01-04 20:15:17 -0500

    Trying to figure out the best way to store inputs and devices. Relying on unity system just to catch the current device an d the current input pressed

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,14 +1,50 @@
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+//public interface IInputController
+//{
+//    public IInputMap inputMap
+//}
+
+public interface IInputDevice
+{
+    public string DeviceId { get; }
+}
+
+public class XboxInputDevice : IInputDevice
+{
+    public string DeviceId => "";
+}
+
+public class PcInputDevice : IInputDevice
+{
+    public string DeviceId => throw new System.NotImplementedException();
+}
+
+public class PlayStationInputDevice : IInputDevice
+{
+    public string DeviceId => throw new System.NotImplementedException();
+}
+
+public class InputDictionary
+{
+    public const string AttackInputId = "attack_input";
+    public const string InteractInputId = "interact_input";
+    public const string JumpInputId = "jump_input";
+    public const string MoveForwardInputId = "move_forward_input";
+    public const string MoveBackInputId = "move_back_input";
+    public const string MoveRightInputId = "move_right_input";
+    public const string MoveLeftInputId = "move_left_input";
+}
 
 public class InputManager
 {
-    public static string AttackInput = "q";
-    public static string InteractInput = "e";
-    public static string JumpInput = " ";
-    public static string MoveForwardInput = "w";
-    public static string MoveBackInput = "s";
-    public static string MoveRightInput = "d";
-    public static string MoveLeftInput = "a";
+    public const string PC_INPUT_DEVICE_ID = "pc";
+    public const string PLAY_STATION_INPUT_DEVICE_ID = "ps";
+    public const string XBOX_INPUT_DEVICE_ID = "xbox";
+
+    private static IInputDevice m_currentDevice;
+    public static Dictionary<string, string> InputMap;
 
     public static bool WasInputUsed(string assignedInput, string usedInput) {
         // gURADAR EL INPUT EN lower case para evitar hacer el split y tener que gaurdar mas de un input con la separacion
@@ -24,22 +60,36 @@ public class InputManager
         return false;
     }
 
+    //public static void UpdateInputs() {
+    //    Keyboard.current.b
+    //}
+
+    public static void ChangeDevice(IInputDevice device) {
+        m_currentDevice = device;
+
+        UpdateInputMap();
+    }
+
+    private static void UpdateInputMap() {
+        // Ned to create a json with a default config of input and override it when user change input inside game
+    }
+
     public static string GetInputByAction(string actionId) {
         switch (actionId) {
             case ActionsDictionary.ATTACK_MELEE_ACTION_ID:
-                return AttackInput;
+                return InputMap[InputDictionary.AttackInputId];
             case ActionsDictionary.INTERACT_ACTION_ID:
-                return InteractInput;
+                return InputMap[InputDictionary.InteractInputId];
             case ActionsDictionary.JUMP_ACTION_ID:
-                return JumpInput;
+                return InputMap[InputDictionary.JumpInputId];
             case ActionsDictionary.MOVE_FORWARD_ACTION_ID:
-                return MoveForwardInput;
+                return InputMap[InputDictionary.MoveForwardInputId];
             case ActionsDictionary.MOVE_BACK_ACTION_ID:
-                return MoveBackInput;
+                return InputMap[InputDictionary.MoveBackInputId];
             case ActionsDictionary.MOVE_LEFT_ACTION_ID:
-                return MoveLeftInput;
+                return InputMap[InputDictionary.MoveLeftInputId];
             case ActionsDictionary.MOVE_RIGHT_ACTION_ID:
-                return MoveRightInput;
+                return InputMap[InputDictionary.MoveRightInputId];
             default:
                 return "";
         }
